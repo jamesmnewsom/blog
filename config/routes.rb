b800501c81a778b1280111431f4fb017d2b3495c
James Newsom <jamesmnewsom@gmail.com>
2015-02-21 16:28:35 -0600

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,8 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :articles
+  #resources :articles
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
