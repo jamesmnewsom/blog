b510bbe5ade82b8337ee77751ccef7713c856a0f
James Newsom <jamesmnewsom@gmail.com>
2015-02-22 11:47:58 -0600

    complete log-in/log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+
   #get 'users/new'
 
   #get 'static_pages/home'
@@ -17,6 +19,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
