263cb505626a91fc93abeb4ff2aa7d684d5f461a
James Newsom <jamesmnewsom@gmail.com>
2015-03-21 21:27:17 -0500

    Finish user edit, update, destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(firstname:  "Example",
+			 lastname:  "User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  firstname  = Faker::Name.name
+  lastname = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(firstname:  firstname,
+  			   lastname:  lastname,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
+      post users_path, user: { firstname:  "",
+      						             lastname: "",
                                email: "user@invalid",
                                password:              "foo",
                                password_confirmation: "bar" }
@@ -14,16 +15,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
-	get signup_path
-	assert_difference 'User.count', 1 do
-	  post_via_redirect users_path, user: { firstname:  "Example",
-	  										lasname: "User",
-	                                        email: "user@example.com",
-	                                        password:              "password",
-	                                        password_confirmation: "password" }
-	end
-	assert_template 'users/show'
-	assert is_logged_in?
+  	get signup_path
+  	assert_difference 'User.count', 1 do
+  	  post_via_redirect users_path, user: { firstname:  "Example",
+  	  										                  lasname: "User",
+  	                                        email: "user@example.com",
+  	                                        password:              "password",
+  	                                        password_confirmation: "password" }
+    end
+	  assert_template 'users/show'
+	  assert is_logged_in?
   end
-
 end
